00a150e75ba0b68ddb3e6ed07f48b92c541c5dd5
Manu <manualta@hotmail.it>
2017-07-31 17:17:59 +0200

    Second commit

--- assignment.js
+++ assignment.js
@@ -4,11 +4,12 @@ var prompt = require('prompt');
 var readline = require('readline');
 
 
+
 showMenu()
 
 function showMenu() {
   prompt.start();
-  console.log('Select your choice:\n1)Insert\n2)Remove\n3)Update\n4)Show');
+  console.log('Select your choice:\n1)Insert\n2)Remove\n3)Update\n4)Show\n5)Exit');
   prompt.get(["Choice"], function (err, result) {
     if (err) console.log('Sorry impossibile to connect to MongoDB. Errore:', err);
     switch (result.Choice) {
@@ -20,6 +21,8 @@ function showMenu() {
         break;
       case "4": showData();
         break;
+      case "5": exit();
+        break;    
     }
   })
 }
@@ -34,18 +37,25 @@ function insertData() {
         output: process.stdout
       });
 
+       
     r1.on('close', function () {
-      console.log('Goodbye!');
-      process.exit(0);
+      
+      console.log('What would you like to do? ');
+      showMenu();
+      //process.exit(0);
+      
     });
-
+    
+      
     r1.question('Enter the smarthphone brand:', function (brand) {
       r1.question('Enter the model:', function (type) {
 
         db.collection('smartphone').insert({ 'brand': brand, 'type': type });
         console.log("Done");
+       
         db.close();
         r1.close();
+        
       });
     });
 
@@ -61,16 +71,19 @@ function deleteData() {
         input: process.stdin,
         output: process.stdout
       });
+       
+    r1.on('close', function () {
 
-       r1.on('close', function () {
-      console.log('Goodbye!');
-      process.exit(0);
+      console.log('What would you like to do? ');
+      showMenu();
+      //process.exit(0);
         });
+
     r1.question('Enter the smarthphone brand you want remove:', function (brand) {
       r1.question('Enter the model:', function (type) {
 
         db.collection('smartphone').deleteOne({ 'brand': brand, 'type': type });
-        var collection = db.collection('smartphone').find();
+        //var collection = db.collection('smartphone').find();
         console.log("Done");
         db.close();
         r1.close();
@@ -93,8 +106,9 @@ function showData() {
 
 
     r1.on('close', function () {
-      console.log('Goodbye!');
-      process.exit(0);
+      console.log('What would you like to do? ');
+      showMenu();
+      //process.exit(0);
     });
 
     db.collection("smartphone").find({}, { _id: false }).limit(10).toArray(function (err, result) {
@@ -118,13 +132,15 @@ function updateData() {
       });
 
        r1.on('close', function () {
-      console.log('Goodbye!');
-      process.exit(0);
+      console.log('What would you like to do? ');
+      showMenu();
+      //process.exit(0);
         });
 
 r1.question('Enter the brand and model you want to change:', function (type) {
       r1.question('Enter the new model:', function (type1) {
         query='{"brand":"'+type.split(',')[0]+'","type":"'+type.split(',')[1]+'"}'
+        //creating file jason 
         console.log(query);
         
         query1='{"brand":"'+type1.split(',')[0]+'","type":"'+type1.split(',')[1]+'"}'
@@ -142,3 +158,25 @@ r1.question('Enter the brand and model you want to change:', function (type) {
   });
 }
 
+function exit() {
+
+  MongoClient.connect(url, function (err, db) {
+    if (err) console.log('Sorry impossibile to connect to MongoDB. Errore:', err);
+   
+    var r1 = readline.createInterface(
+      {
+        input: process.stdin,
+        output: process.stdout
+      });
+
+       r1.on('close', function () {
+      console.log('Goodbye!');
+      process.exit(0);
+        });
+        
+        db.close();
+        r1.close();
+
+      });
+  
+}
